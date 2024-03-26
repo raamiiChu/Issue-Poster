1ebb0aef5b7fd4ca3b5ac1874b1f3eab1cfc4e00
raamiiChu <ericchu0617@gmail.com>
2024-03-26 18:43:24 +0800

    feat(header): add theme toggle

---
+++ src/components/Header/ThemeToggle.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import React from "react";
+import { useThemeContext } from "@/context/ThemeContext";
+
+import { CiLight } from "react-icons/ci";
+import { LuMoonStar } from "react-icons/lu";
+
+const ThemeToggle = () => {
+    const { theme, setTheme } = useThemeContext();
+
+    const switchTheme = (e: any) => {
+        e.preventDefault();
+
+        setTheme(theme === "dark" ? "light" : "dark");
+    };
+
+    return (
+        <button className="group" onClick={switchTheme}>
+            {theme === "light" ? (
+                <CiLight className="size-9 text-black group-hover:opacity-75" />
+            ) : (
+                <LuMoonStar className="size-9 text-white group-hover:opacity-75" />
+            )}
+        </button>
+    );
+};
+
+export default ThemeToggle;
