827a07c5d62e1b5350708e20d8df4770d319564a
raamiiChu <ericchu0617@gmail.com>
2024-03-26 18:41:48 +0800

    feat: github oauth provider

---
+++ src/app/[api]/auth/[[...nextauth]]/route.tsx
@@ -0,0 +1,33 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+const { GITHUB_ID, GITHUB_SECRET } = process.env;
+
+const handler = NextAuth({
+    // Configure one or more authentication providers
+    providers: [
+        GithubProvider({
+            clientId: GITHUB_ID,
+            clientSecret: GITHUB_SECRET,
+        }),
+    ],
+    callbacks: {
+        async session({ session, token, user }) {
+            session.user.id = token.id;
+            session.accessToken = token.accessToken;
+            return session;
+        },
+        async jwt({ token, user, account, profile, isNewUser }) {
+            if (user) {
+                token.id = user.id;
+            }
+            if (account) {
+                token.accessToken = account.access_token;
+            }
+            return token;
+        },
+    },
+    pages: { error: "/" },
+});
+
+export { handler as GET, handler as POST };
