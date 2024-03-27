7ae9ea79f19a66747dcf40894a6900dec401b643
raamiiChu <ericchu0617@gmail.com>
2024-03-27 13:49:54 +0800

    feat(header): add github avatar

---
+++ src/components/Header/GithubAvatar.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+import { useSession } from "next-auth/react";
+
+const GithubAvatar = () => {
+    const { data: session, status } = useSession();
+
+    if (status !== "authenticated") {
+        return <div className=""></div>;
+    }
+
+    const { name, image } = session.user;
+
+    return (
+        <Link
+            href={`https://github.com/${name}`}
+            className="relative size-9 hover:opacity-50 transition-all duration-500"
+        >
+            <Image
+                src={image}
+                fill={true}
+                alt={name}
+                className="rounded-full"
+            />
+        </Link>
+    );
+};
+
+export default GithubAvatar;
