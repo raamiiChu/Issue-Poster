0527114ced6b24e9ba3c5c9bf86b077dd100ef67
raamiiChu <ericchu0617@gmail.com>
2024-03-27 16:06:08 +0800

    feat(homepage): add link to issue page

--- src/components/RepoCards/RepoCards.tsx
+++ src/components/RepoCards/RepoCards.tsx
@@ -1,19 +1,12 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React from "react";
 import Link from "next/link";
+import Image from "next/image";
+import { Repo } from "@/types";
 
 import { FaLink } from "react-icons/fa";
 import { format } from "date-fns";
-
-interface Repo {
-    id: number;
-    name: string;
-    owner: { login: string; html_url: string };
-    html_url: string;
-    description: string;
-    created_at: string;
-}
 
 interface Params {
     repos: Repo[];
@@ -21,7 +14,7 @@ interface Params {
 
 const RepoCards = ({ repos }: Params) => {
     return (
-        <ul className="col-start-3 col-span-8 grid grid-cols-1 gap-y-5 divide-black dark:divide-white">
+        <ul className="col-start-3 col-span-8 grid grid-cols-1 gap-y-5">
             {repos?.map((repo: Repo, index) => {
                 const { id, name, owner, html_url, description, created_at } =
                     repo;
@@ -31,38 +24,58 @@ const RepoCards = ({ repos }: Params) => {
                         key={id}
                         className={`p-8 grid grid-cols-12 items-center gap-2.5 ${
                             index % 2 === 1
-                                ? "bg-blue-300 dark:bg-blue-800"
+                                ? "bg-blue-200 dark:bg-blue-900"
                                 : "bg-slate-300 dark:bg-slate-800"
                         }`}
                     >
-                        <section className="col-span-6">
-                            <section className="flex gap-5">
-                                <h2 className="text-xl font-bold">{name}</h2>
-                                <Link
-                                    href={html_url}
-                                    title={name}
-                                    target="blank"
-                                    className="group flex items-center"
-                                >
-                                    <FaLink className="text-xl group-hover:opacity-75" />
-                                </Link>
-                            </section>
-
-                            <h3
-                                title={description || name}
-                                className="line-clamp-2"
+                        <section className="col-span-6 flex gap-x-2.5">
+                            <h2
+                                className="text-xl font-bold line-clamp-1"
+                                title={name}
                             >
-                                {description || name}
-                            </h3>
+                                {name}
+                            </h2>
+                            <Link
+                                href={html_url}
+                                title={name}
+                                target="blank"
+                                className="flex items-center hover:opacity-50 transition-all duration-500"
+                            >
+                                <FaLink className="text-xl" />
+                            </Link>
                         </section>
 
-                        {/* href */}
-                        <section className="col-span-6"></section>
+                        <section className="col-span-6">
+                            <Link href={`/issues/${owner.login}/${name}`}>
+                                See Issues
+                            </Link>
+                        </section>
+
+                        <p
+                            title={description || name}
+                            className="col-span-full text-sm line-clamp-2"
+                        >
+                            {description || name}
+                        </p>
 
                         <section className="col-span-full flex justify-between">
-                            <Link href={owner.html_url} target="blank">
-                                <p>Created By: {owner.login}</p>
-                            </Link>
+                            <section className="flex items-center gap-x-2.5">
+                                <span>Created By:</span>
+                                <Link
+                                    href={owner.html_url}
+                                    target="blank"
+                                    className="size-6 relative hover:opacity-50 transition-all duration-500"
+                                >
+                                    <Image
+                                        src={owner.avatar_url}
+                                        fill={true}
+                                        alt={owner.login}
+                                        className="rounded-full"
+                                    />
+                                </Link>
+                                <span>{owner.login}</span>
+                            </section>
+
                             <small className="text-sm">
                                 Created At:{" "}
                                 {format(new Date(created_at), "yyyy / MM / dd")}
