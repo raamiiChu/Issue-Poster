2901146914cf787727f6dd2025c4d9dbe0a6be33
raamiiChu <ericchu0617@gmail.com>
2024-03-27 16:08:35 +0800

    feat(issuePage): get issue data & show markdown

---
+++ src/app/(auth)/issues/[...details]/page.tsx
@@ -0,0 +1,85 @@
+"use client";
+
+import React, { useState } from "react";
+import { IssuePageParams, Issue } from "@/types";
+
+import axios from "axios";
+import { useSession } from "next-auth/react";
+import useSWR from "swr";
+
+import Markdown from "react-markdown";
+
+interface Params {
+    params: IssuePageParams;
+}
+
+const fetcher = async (url: string, token: string) => {
+    try {
+        const { data, status } = await axios.get(url, {
+            headers: { Authorization: `bearer ${token}` },
+        });
+
+        if (status === 200) {
+            return data as Issue[];
+        }
+
+        return [];
+    } catch (error) {
+        console.log(error);
+        return [];
+    }
+};
+
+const IssuePage = ({ params }: Params) => {
+    const [owner, repo] = params.details;
+    const { data: session } = useSession();
+    // const { data: issues }: { data: Issue[] } = await axios.get(
+    //     `https://api.github.com/repos/${owner}/${repo}/issues`
+    // );
+
+    // console.log(issues);
+
+    const {
+        data: issues,
+        mutate,
+        error,
+        isLoading,
+        isValidating,
+    } = useSWR(
+        [
+            `https://api.github.com/repos/${owner}/${repo}/issues?per_page=10&page=1`,
+            session?.accessToken,
+        ],
+        ([url, token]) => fetcher(url, token)
+    );
+
+    return (
+        <main className="min-h-screen grid grid-cols-12 gap-10 px-12">
+            {issues?.map((issue) => {
+                const {
+                    id,
+                    number,
+                    html_url,
+                    title,
+                    body,
+                    comments,
+                    comments_url,
+                } = issue;
+
+                return (
+                    <div key={id} className="grid col-span-full">
+                        <h1>{title}</h1>
+
+                        <span>{html_url}</span>
+
+                        <div className="mx-auto prose prose-init prose-stone dark:prose-invert">
+                            <Markdown>{body}</Markdown>
+                        </div>
+                    </div>
+                );
+            })}
+        </main>
+    );
+};
+
+export default IssuePage;
