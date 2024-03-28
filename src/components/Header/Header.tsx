76f92b5d255cadf048d707733a9516a8e0d38b36
raamiiChu <ericchu0617@gmail.com>
2024-03-28 14:42:27 +0800

    feat(header): add link to homepage

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -10,7 +10,9 @@ import ThemeToggle from "./ThemeToggle";
 const Header = () => {
     return (
         <header className="sticky top-0 z-50 h-16 grid grid-cols-12 px-12 items-center border-b border-slate-500 backdrop-blur">
-            <h1 className="col-span-3 text-2xl font-bold">LOGO</h1>
+            <Link href={"/"} className="col-span-3 text-2xl font-bold">
+                <h1>Home</h1>
+            </Link>
             <nav className="col-end-13 col-span-2 grid grid-cols-4 items-center gap-5">
                 <GithubAvatar />
                 <ThemeToggle />
