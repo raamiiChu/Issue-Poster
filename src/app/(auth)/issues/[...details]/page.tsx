46584de568d61b735671075caec4285a5b826bf4
raamiiChu <ericchu0617@gmail.com>
2024-03-28 21:04:07 +0800

    style(issuesPage): page layout

--- src/app/(auth)/issues/[...details]/page.tsx
+++ src/app/(auth)/issues/[...details]/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
+import Link from "next/link";
 import { IssuePageParams, Issue } from "@/types";
 
 import axios from "axios";
@@ -8,6 +9,9 @@ import { useSession } from "next-auth/react";
 import useSWR from "swr";
 
 import Markdown from "react-markdown";
+
+import { BsBoxArrowUpRight } from "react-icons/bs";
+import { format } from "date-fns";
 
 interface Params {
     params: IssuePageParams;
@@ -45,30 +49,72 @@ const IssuePage = ({ params }: Params) => {
     );
 
     return (
-        <main className="min-h-screen grid grid-cols-12 gap-10 px-12">
-            {issues?.map((issue) => {
-                const {
-                    id,
-                    number,
-                    html_url,
-                    title,
-                    body,
-                    comments,
-                    comments_url,
-                } = issue;
+        <main className="container min-h-screen grid grid-cols-12 gap-10 mx-auto px-12 pt-4 pb-16">
+            <h1 className="col-start-2 col-span-10 mt-5 text-4xl font-bold">
+                Issues in {repo}
+            </h1>
 
-                return (
-                    <div key={id} className="grid col-span-full">
-                        <h1>{title}</h1>
+            {issues?.length === 0 && (
+                <article className="col-span-full space-y-5 text-center">
+                    <h2 className="text-3xl">
+                        Oops, there&apos;s no issues in this repo.
+                    </h2>
+                    <p className="text-xl">You can post a new issue.</p>
+                </article>
+            )}
 
-                        <span>{html_url}</span>
+            <button className="h-fit col-start-6 col-span-2 px-5 py-2.5 border border-black rounded bg-white text-black font-bold hover:opacity-50 transition-all duration-500">
+                Post an Issue
+            </button>
 
-                        <div className="mx-auto prose prose-init prose-stone dark:prose-invert">
-                            <Markdown>{body}</Markdown>
-                        </div>
-                    </div>
-                );
-            })}
+            <section className="h-fit col-start-2 col-span-10 grid grid-cols-12 gap-x-10 gap-y-16">
+                {issues?.map((issue) => {
+                    const {
+                        id,
+                        number,
+                        html_url,
+                        title,
+                        body,
+                        comments,
+                        comments_url,
+                    } = issue;
+
+                    return (
+                        <article
+                            key={id}
+                            className="col-span-4 flex flex-col justify-between gap-y-5 p-6 border-2 rounded border-black dark:border-white"
+                        >
+                            <section className="grid grid-cols-12 justify-between items-center gap-x-4">
+                                <Link
+                                    className="group col-span-10 flex items-center gap-x-4 text-2xl font-bold line-clamp-2"
+                                    href={html_url}
+                                >
+                                    <h2
+                                        title={title}
+                                        className="underline-offset-2 group-hover:underline transition-all"
+                                    >
+                                        {title}
+                                    </h2>
+
+                                    <BsBoxArrowUpRight className="text-black dark:text-white group-hover:opacity-50 transition-all" />
+                                </Link>
+
+                                <span className="col-span-2 text-center">
+                                    # {number}
+                                </span>
+                            </section>
+
+                            <button className="h-fit px-5 py-1.5 border border-black rounded bg-white text-black font-bold hover:opacity-50 transition-all duration-500">
+                                Comments: {comments}
+                            </button>
+
+                            {/* <div className="mx-auto prose prose-init prose-stone dark:prose-invert">
+                                <Markdown>{body}</Markdown>
+                            </div> */}
+                        </article>
+                    );
+                })}
+            </section>
         </main>
     );
 };
