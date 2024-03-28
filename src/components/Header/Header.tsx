237e31e7f9836ac7a87ea94b1142bb9f8f14d130
raamiiChu <ericchu0617@gmail.com>
2024-03-28 21:04:52 +0800

    style(header): modify css style

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,8 +9,11 @@ import ThemeToggle from "./ThemeToggle";
 
 const Header = () => {
     return (
-        <header className="sticky top-0 z-50 h-16 grid grid-cols-12 px-12 items-center border-b border-slate-500 backdrop-blur">
-            <Link href={"/"} className="col-span-3 text-2xl font-bold">
+        <header className="container sticky top-0 z-50 h-16 grid grid-cols-12 mx-auto px-12 items-center border-b border-slate-500 backdrop-blur">
+            <Link
+                href={"/"}
+                className="col-span-1 text-2xl font-bold hover:opacity-50 transition-all duration-500"
+            >
                 <h1>Home</h1>
             </Link>
             <nav className="col-end-13 col-span-2 grid grid-cols-4 items-center gap-5">
