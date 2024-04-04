645381988b0c8788c31feb917ad3215b6a393cf5
raamiiChu <ericchu0617@gmail.com>
2024-04-04 22:56:12 +0800

    feat(issue api): post an new issue

--- src/app/api/github/issues/[owner]/[repo]/route.ts
+++ src/app/api/github/issues/[owner]/[repo]/route.ts
@@ -31,6 +31,8 @@ export const GET = async (request: any, { params }: IssuePageParams) => {
         switch (status) {
             case 301:
                 return new NextResponse("Moved permanently", { status });
+            case 401:
+                return new NextResponse("Unauthorized", { status });
             case 404:
                 return new NextResponse("Not found", { status });
             case 422:
@@ -38,6 +40,56 @@ export const GET = async (request: any, { params }: IssuePageParams) => {
                     "Validation failed, or the endpoint has been spammed",
                     { status }
                 );
+            default:
+                return new NextResponse("Client side error", { status: 400 });
+        }
+    }
+};
+
+export const POST = async (request: any, { params }: IssuePageParams) => {
+    const { owner, repo } = params;
+    const githubToken = request.headers.get("Authorization");
+
+    // body
+    const { title, body } = await request.json();
+
+    if (!githubToken) {
+        return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    try {
+        const res = await axios.post(
+            `https://api.github.com/repos/${owner}/${repo}/issues`,
+            { title, body },
+            { headers: { Authorization: `Bearer ${githubToken}` } }
+        );
+
+        const { status } = res;
+
+        if (status === 201) {
+            return new NextResponse("Issue has been created successfully", {
+                status,
+            });
+        }
+    } catch (error: any) {
+        const { status } = error.response;
+
+        switch (status) {
+            case 401:
+                return new NextResponse("Unauthorized", { status });
+            case 403:
+                return new NextResponse("Forbidden", { status });
+            case 404:
+                return new NextResponse("Resource not found", { status });
+            case 410:
+                return new NextResponse("Gone", { status });
+            case 422:
+                return new NextResponse(
+                    "Validation failed, or the endpoint has been spammed.",
+                    { status }
+                );
+            case 503:
+                return new NextResponse("Service unavailable", { status });
             default:
                 return new NextResponse("Client side error", { status: 400 });
         }
