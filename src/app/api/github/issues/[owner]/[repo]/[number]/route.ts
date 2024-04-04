ebe5bbde80a9669b10b8934c7dd3c8b9582af548
raamiiChu <ericchu0617@gmail.com>
2024-04-04 17:41:00 +0800

    feat(issue api): close (delete) issue

--- src/app/api/github/issues/[owner]/[repo]/[number]/route.ts
+++ src/app/api/github/issues/[owner]/[repo]/[number]/route.ts
@@ -41,3 +41,53 @@ export const GET = async (request: any, { params }: IssueNumberPageParams) => {
         }
     }
 };
+
+export const DELETE = async (
+    request: any,
+    { params }: IssueNumberPageParams
+) => {
+    const url = new URL(request.url);
+
+    const { owner, repo, number } = params;
+    const githubToken = request.headers.get("Authorization");
+
+    if (!githubToken) {
+        return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    try {
+        const res = await axios.patch(
+            `https://api.github.com/repos/${owner}/${repo}/issues/${number}`,
+            { state: "close" },
+            { headers: { Authorization: `Bearer ${githubToken}` } }
+        );
+
+        const { data, status } = res;
+
+        if (status === 200) {
+            return new NextResponse(JSON.stringify(data), { status });
+        }
+    } catch (error: any) {
+        const { status } = error.response;
+
+        switch (status) {
+            case 301:
+                return new NextResponse("Moved permanently", { status });
+            case 403:
+                return new NextResponse("Forbidden", { status });
+            case 404:
+                return new NextResponse("Resource not found", { status });
+            case 410:
+                return new NextResponse("Gone", { status });
+            case 422:
+                return new NextResponse(
+                    "Validation failed, or the endpoint has been spammed.",
+                    { status }
+                );
+            case 503:
+                return new NextResponse("Service unavailable", { status });
+            default:
+                return new NextResponse("Client side error", { status: 400 });
+        }
+    }
+};
