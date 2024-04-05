003879ce13addaad651370bb70dcee182bf28915
raamiiChu <ericchu0617@gmail.com>
2024-04-05 20:29:59 +0800

    build: ignore build errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: { domains: ["avatars.githubusercontent.com"] },
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
+    },
 };
 
 export default nextConfig;
