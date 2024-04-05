086fd56388910b42136257a7700f9fd045536c17
raamiiChu <ericchu0617@gmail.com>
2024-04-05 20:48:06 +0800

    fix(homepage): loading status

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,15 @@ import { useSession, signIn } from "next-auth/react";
 export default function Home() {
     const { data: session, status } = useSession();
 
+    if (status === "loading") {
+        return (
+            <main className="container min-h-screen mx-auto py-12 space-y-6">
+                <h2 className="text-3xl text-center font-bold ">Loading...</h2>
+                <div className="col-span-full animate-spin size-12 mx-auto my-12 border-b-4 border-slate-900 dark:border-white rounded-full"></div>
+            </main>
+        );
+    }
+
     if (status === "unauthenticated") {
         return (
             <main className="container min-h-screen mx-auto py-12 space-y-6">
